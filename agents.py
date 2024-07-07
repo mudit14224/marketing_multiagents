95084d216c982971f3c7e1e53ee080dadbce193e
mudit14224 <mudit.jindal14224@gmail.com>
2024-07-07 11:45:22 -0500

    Added agents class

---
+++ agents.py
@@ -0,0 +1,58 @@
+# Imports
+from crewai import Agent, Task, Crew, Process
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool, WebsiteSearchTool
+
+
+class MarketingAgents: 
+    def __init__(self, url):
+        self.url = url
+        self.serper_tool = SerperDevTool()
+        self.scrape_tool = ScrapeWebsiteTool(website_url=self.url)
+        self.website_search_rag_tool = WebsiteSearchTool()
+
+    def create_query_agents(self):
+        # Information Specialist
+        information_specialist = Agent(
+            role = 'Information Specialist',
+            goal = 'Gather all information about a healthcare provider',
+            backstory = 
+                """You work for a digital healthcare company called as Healee.
+                You are an expert in detailed research and data compilation.
+                You have to find out all the important information possible about {client} healthcare provider.""",
+            tools = [self.scrape_tool],
+            verbose = True,
+            allow_delegation=False
+        )
+
+        # Query Agent
+        search_query_generator = Agent(
+        role='Search Query Generator',
+        goal='Generate search queries to find similar healthcare providers companies to {client} based on provided criteria and gathered information',
+        backstory='Proficient in creating effective search queries from given data.',
+        allow_delegation=False, 
+        verbose = True
+        )
+
+        # Final Consolidator Agent
+        consolidator = Agent(
+            role='Result Consolidator',
+            goal='Combine all the results from the researcher agents into a final list of healthcare companies.',
+            backstory='You excel at synthesizing information from multiple sources into a clear, concise list which has the company name and also the website link.',
+            verbose=True,
+            allow_delegation=False
+        )
+
+        return information_specialist, search_query_generator, consolidator
+
+    # Researcher Agent function
+    def create_researcher_agent(self, query):
+        return Agent(
+            role='Internet Researcher',
+            goal=f'Find similar healthcare provider companies using the query: {query}',
+            backstory="""You work in the marketing team of a digital healthcare company.
+                        You are adept at using online resources to find relevant information quickly.
+                        You are looking for potential healthcare provider clients similar to {client}
+                        that your company can pitch your platform to and sign them.""",
+            tools=[self.serper_tool],
+            allow_delegation=False
+        )
